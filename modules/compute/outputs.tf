396e0b13ddebb246934dff3327bf740b40ee08d5
GMG-bit <gas.mardones@duocuc.cl>
2026-04-28 01:09:56 -0400

    refactor: remove output for instance IPs from outputs.tf

--- modules/compute/outputs.tf
+++ modules/compute/outputs.tf
@@ -1,8 +1,3 @@
-output "instance_ips" {
-  description = "IPs públicas de las instancias creadas"
-  value       = aws_instance.server[*].public_ip
-}
-
 output "instance_ids" {
   description = "IDs de las instancias creadas"
   value       = aws_instance.server[*].id
